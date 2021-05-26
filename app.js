bf245da6edf27883d6ba19feabec8f911b82c30e
tabishismail <tabish554@gmail.com>
2021-05-26 20:58:15 +0500

    Assignment # 8 Done

--- app.js
+++ app.js
@@ -59,3 +59,18 @@
 // var xyz= parseFloat(abc);
 // document.write("<div>","The weight of user is: " +xyz+" kilograms"+"</div>");
 
+// Assignment # 8
+
+// var user = +prompt("Enter number between 1 to 10")
+// var randomNum = Math.round(Math.random() * 10);
+// if(randomNum===0){
+//     randomNum = 1;
+// }
+
+// else if(randomNum===user){
+//     alert("Congratulation!! You have enter correct secret number ");
+// }
+// else{
+//     alert("Sorry !! Better luck next time");
+    
+// }
