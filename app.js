775f36118a12524fe1767e0624cefd767acfbfc2
tabishismail <tabish554@gmail.com>
2021-05-26 19:58:36 +0500

    Assignment # 1 done

---
+++ app.js
@@ -0,0 +1,10 @@
+// Chapter 26 to 30 Assignments
+
+// Assignment # 1
+
+// var user= +prompt("Enter any integer");
+
+// document.write("<div>","Number: "+user+"</div>");
+// document.write("<div>","Round off value: "+Math.round(user)+"</div>");
+// document.write("<div>","Floor off value: "+Math.floor(user)+"</div>");
+// document.write("<div>","Ceil off value: "+Math.ceil(user)+"</div>");
