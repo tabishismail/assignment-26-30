8c84bc87a2423edc6a5fad1da79175a840d486be
tabishismail <tabish554@gmail.com>
2021-05-26 20:08:18 +0500

    Assignment # 2 Done

--- app.js
+++ app.js
@@ -8,3 +8,12 @@
 // document.write("<div>","Round off value: "+Math.round(user)+"</div>");
 // document.write("<div>","Floor off value: "+Math.floor(user)+"</div>");
 // document.write("<div>","Ceil off value: "+Math.ceil(user)+"</div>");
+
+// // Assignment # 2
+
+// var user= +prompt("Enter any integer");
+
+// document.write("<div>","Number: "+user+"</div>");
+// document.write("<div>","Round off value: "+Math.round(user)+"</div>");
+// document.write("<div>","Floor off value: "+Math.floor(user)+"</div>");
+// document.write("<div>","Ceil off value: "+Math.ceil(user)+"</div>");
